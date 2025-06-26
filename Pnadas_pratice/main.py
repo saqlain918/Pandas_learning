2d7a1824a38c6391609a05ac428dc50ee6e47f4d
Rai Saqlain <136235749+saqlain918@users.noreply.github.com>
2025-06-26 17:41:18 +0500

    Delete Pnadas_pratice directory

--- Pnadas_pratice/main.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-df = pd.read_csv("Students Social Media Addiction.csv")
-
-print(df.columns)
-# Total number of records (rows)
-total_records = len(df)
-print("Total records:", total_records)
-
